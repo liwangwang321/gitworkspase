2f64e795c0d35de7cd58e1223459b1cb53ceacf7
liwangwang321 <1152783821@qq.com>
2019-10-17 14:56:58 +0800

    把一个demo放到上面去了

---
+++ ssm/src/main/java/com/liwangwang/demo.java
@@ -0,0 +1,14 @@
+package com.liwangwang;
+
+/**
+ * @authorliwangwang
+ * @site www.liwangwang.com
+ * @company xxx公司
+ * @create 2019-10-17 14:55
+ */
+public class demo {
+    public static void main(String[] args) {
+
+        System.out.println("到了哦");
+    }
+}
